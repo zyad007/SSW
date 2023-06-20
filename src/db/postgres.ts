2e4f01ccb24986989c30b631ed3d092d2fb0ea3c
Zyad Abdul-Nasser <zizo_007@live.com>
2023-06-20 17:59:20 +0300

    z

--- src/db/postgres.ts
+++ src/db/postgres.ts
@@ -1,11 +1,19 @@
 import { Pool } from "pg";
 
+// const pool = new Pool({
+//     host: 'localhost',
+//     port: 5432,
+//     user: 'postgres',
+//     password: 'postgres',
+//     database: 'sw-2'
+// });
+
 const pool = new Pool({
-    host: 'localhost',
+    host: 'surus.db.elephantsql.com',
     port: 5432,
-    user: 'postgres',
-    password: 'postgres',
-    database: 'sw-2'
+    user: 'nysdhenx',
+    password: '9PNYSlRuFQGeU4rKF2MeizbB5PEjHJ-N',
+    database: 'nysdhenx'
 });
 
 export default pool;
